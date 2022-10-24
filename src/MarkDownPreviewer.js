32c2a9b70f5724d00145b271a6a8c9968ae3ec6b
Jem Herrera <jhneramuz@gmail.com>
2022-10-24 10:57:50 -0600

    cleaned default markdown text

--- src/MarkDownPreviewer.js
+++ src/MarkDownPreviewer.js
@@ -3,12 +3,46 @@ import Editor from './Editor';
 import Previewer from './Previewer';
 
 const pixel = require('./static/pixelpfp.jpg');
+const defaultMarkdown = [
+  "# Welcome to my Markdown Previewer!\n",
+  "## This is a project made using React..",
+  "### If you don't know how to use this app, just start typing on the Editor:\n",
+  "Here's a few examples:\n",
+  "Heres some inline code, `<div></div>`, between 2 backticks.\n",
+  "```\n",
+  "// this is multi-line code:\n",
+  "function anotherExample(firstLine, lastLine) {",
+  "  if (firstLine == '```' && lastLine == '```') {",
+  "    return multiLineCode;",
+  "  }",
+  "}",
+  "```\n",
+  "You can also make text **bold**... whoa!",
+  "Or _italic_.",
+  "Or... wait for it... **_both!_**",
+  "And feel free to go crazy ~~crossing stuff out~~.\n",
+  "There's also [links](https://jemherrera.github.io/portfolio/), and",
+  "> Block Quotes!\n",
+  "And if you want to get really crazy, even tables:\n",
+  "Wild Header | Crazy Header | Another Header?",
+  "------------ | ------------- | -------------",
+  "Your content can | be here, and it | can be here....",
+  "And here. | Okay. | I think we get it.\n",
+  "- And of course there are lists.",
+  "  - Some are bulleted.",
+  "     - With different indentation levels.",
+  "        - That look like this.\n\n",
+  "1. And there are numbered lists too.",
+  "1. Use just 1s if you want!",
+  "1. And last but not least, let's not forget embedded images:\n",
+  `![My pixel picture](${pixel})`
+]
 
 class MarkDownPreviewer extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      input: "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading..\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![My pixel picture]("+ pixel +")"
+      input: defaultMarkdown.join('\n')
     }
     this.handleChange = this.handleChange.bind(this);
   }
